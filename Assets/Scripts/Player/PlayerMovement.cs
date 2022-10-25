36c476e514b21dcd4b962b93feae97ab51a86874
Denya56 <56lkj546@gmail.com>
2022-10-25 22:40:16 +0200

    added proper dash and 3 types of platforms

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,21 +7,29 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speedR;
     [SerializeField] private float _dash;
     [SerializeField] private float _jumpAccelerationH;
-    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _jumpForce;
     [SerializeField] private LayerMask platformsLayerMask;
-    private Camera _cam;
+    [SerializeField] private float _dashingPower = 24f;
+    [SerializeField] private float _dashingCooldown = 1f;
+
     private float _camHeight;
     private float _camWidth;
     private float _cameraRight;
+    private bool _doubleJump;
+    private float _raycastLength = 0.6f;
+    private bool _canDash = true;
+    
+
+    private Camera _cam;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
-    private BoxCollider2D _boxCollider2D;
+    //private BoxCollider2D _boxCollider2D;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        _boxCollider2D = GetComponent<BoxCollider2D>();
+        //_boxCollider2D = GetComponent<BoxCollider2D>();
         _cam = Camera.main;
         _camHeight = 2f * _cam.orthographicSize;
         _camWidth = _camHeight * _cam.aspect;
@@ -34,12 +43,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButton(Axis.RUN))
             transform.Translate(displacementVector * _speedR * Time.deltaTime);
-        else if (Input.GetButtonDown(Axis.DASH))
+        else if (Input.GetButtonDown(Axis.DASH) && _canDash)
         {
             //Debug.Log(Axis.DASH);
-            transform.Translate(displacementVector * _dash * Time.deltaTime);
+            StartCoroutine(Dash());
+            //transform.Translate(displacementVector * _dash * Time.deltaTime);
         }
-            
         else
             transform.Translate(displacementVector * _speedW * Time.deltaTime);
 
@@ -52,21 +61,49 @@ public class PlayerMovement : MonoBehaviour
         else if (xDisplacement < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
         
-        if (Input.GetButtonDown(Axis.JUMP) && IsGrounded())
-            Jump();
+        if (IsGrounded() && !Input.GetButton(Axis.JUMP))
+        {
+            _doubleJump = false;
+        }
+
+        if (Input.GetButtonDown(Axis.JUMP))
+        {
+            if (IsGrounded() || _doubleJump)
+            {
+                Jump();
+            }
+        }
+            
 
         _animator.SetBool("isWalking", xDisplacement != 0);
         _animator.SetBool("isGrounded", IsGrounded());
 ;   }
 
     private void Jump()
-    {          
-        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x + _jumpAccelerationH, _jumpHeight);
+    {
+        _rigidbody2D.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+        //_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x + _jumpAccelerationH, _jumpHeight);
+        _doubleJump = !_doubleJump;
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+        var raycastHit2D = Physics2D.Raycast(transform.position, Vector2.down, _raycastLength, platformsLayerMask);
+        //RaycastHit2D raycastHit2D = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0f, Vector2.down, _raycastLength, platformsLayerMask);
         return raycastHit2D.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _raycastLength);
+    }
+    private IEnumerator Dash()
+    {
+        _canDash = false;
+        _rigidbody2D.velocity = new Vector2(transform.localScale.x * _dashingPower, 0f);
+        yield return new WaitForSeconds(_dashingCooldown);
+        _canDash = true;
+        
     }
 }
