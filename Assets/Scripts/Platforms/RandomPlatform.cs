f612c3bb79bff95f782de2eb3d4791904d7c9674
Denya56 <56lkj546@gmail.com>
2022-10-26 14:14:13 +0200

    added random platform and new level design

---
+++ Assets/Scripts/Platforms/RandomPlatform.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomPlatform : MonoBehaviour
+{
+    // what is the correct way to do this?
+    private bool _typeSet = false;
+    private int _type = -1;
+    private float _travelDistance = 5;
+    private Vector3 _startPosition;
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        switch (_type)
+        {
+            case 0:
+                transform.position = new Vector3(_startPosition.x + Mathf.PingPong(Time.time, _travelDistance), transform.position.y, transform.position.z);
+                break;
+            case 1:
+                gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!_typeSet)
+        {
+            if (collision.gameObject.CompareTag(Tag.PLAYER))
+            {
+                SetPlatformType();
+            }
+        }
+    }
+
+    private void SetPlatformType()
+    {
+        _typeSet = true;
+        _type = Random.Range(0, 2);
+    }
+}
